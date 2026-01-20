8de6f08540c7dcf5b142fbd32ff55538fc5beaa1
Ankit kumar <ankitkumar.ak0918@gmail.com>
2026-01-21 02:10:20 +0530

    feat: Implement message fetching and sending functionality

--- Backend/src/controllers/message.controllers.js
+++ Backend/src/controllers/message.controllers.js
@@ -3,6 +3,9 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { apiError } from "../utils/apiError.js"
 import apiResponse from "../utils/apiResponse.js"
 import { User } from "../model/user.model.js"
+import { Message } from "../model/message.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+
 
 
 const getAllContact=asyncHandler(async(req,res)=>{
@@ -26,6 +29,67 @@ const getAllContact=asyncHandler(async(req,res)=>{
          )
     )
 })
+
+const getMessageByUserId=asyncHandler(async(req,res)=>{
+    const myId=req.user._id
+    const {id:userToChatId}=req.params
+
+    const message=await Message.find({
+        $or:[
+            {senderId:myId,receiverId:userToChatId},
+            {senderId:userToChatId,receiverId:myId}
+        ]
+    })
+
+    return res.status(200).
+    json(
+        new apiResponse(
+            200,
+            message,
+            "Messages fetched successfully"
+        )
+    )
+})
+
+const sendMessage=asyncHandler(async(req,res)=>{
+    const {text,image}=req.body
+    const {id:receiverId}=req.params
+    const senderId=req.user._id
+
+    console.log("receiverId:",receiverId);
+    console.log("senderId:",senderId);
+    
+    
+
+    let imageUrl
+    if(image){
+        const uploadResponse=await uploadOnCloudinary(image)
+        imageUrl=uploadResponse.url
+    }
+
+    const newMessage=new Message({
+        senderId,
+        receiverId,
+        text,
+        image:imageUrl
+
+    })
+    await newMessage.save()
+    console.log("message:",newMessage);
+     
+    
+
+    return res.status(200).
+    json(
+        new apiResponse(
+            200,
+            newMessage,
+            "Message fetched  successfully"
+        )
+    )
+})
 export {
-    getAllContact
+    getAllContact,
+    getMessageByUserId,
+    sendMessage
 }
--- Backend/src/routes/message.routes.js
+++ Backend/src/routes/message.routes.js
@@ -1,9 +1,15 @@
 import express from "express"
-import { getAllContact } from "../controllers/message.controllers.js"
+import { getAllContact,
+         getMessageByUserId,
+         sendMessage
+ } from "../controllers/message.controllers.js"
 import { verifyJWT } from "../middleware/auth.middleware.js"
+
 
 const router=express.Router()
 
 router.route("/all-contact").get(verifyJWT,getAllContact)
+router.route("/:id").get(verifyJWT,getMessageByUserId)
+router.route("/send/:id").post(verifyJWT,sendMessage)
 
 export default router
