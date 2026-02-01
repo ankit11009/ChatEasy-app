d2da1ee9182d746bba3ce266595289656e9ea88f
Ankit kumar <ankitkumar.ak0918@gmail.com>
2026-02-02 01:37:06 +0530

    fix: update CORS origin handling and set base URL for axios instance

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import  axios from 'axios'
 
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL + "/v1",
   withCredentials: true,
 });
 
