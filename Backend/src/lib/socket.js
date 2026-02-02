b878ef80b6b7828fbdcb5b028b5141a3b33fa4fa
Ankit kumar <ankitkumar.ak0918@gmail.com>
2026-02-03 02:24:49 +0530

    aaaaaa

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -11,7 +11,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
     cors:{
-        origin:process.env.CORS_ORIGIN || process.env.FRONTEND_URL || "http://localhost:5173",
+        origin:"https://chat-easy-app.vercel.app",
         credentials:true,
     }
 })
