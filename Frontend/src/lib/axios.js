2c90670927da15c4144fbeac58391abf84466d18
Ankit kumar <ankitkumar.ak0918@gmail.com>
2026-02-05 01:25:09 +0530

    aaa

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import  axios from 'axios'
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL,
+    https://chateasy-app.onrender.com/api/v1,
   withCredentials: true,
 });
 
